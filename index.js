1c1be413f83ea4077063fd960c14a1b19b3cf0e9
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-02-13 12:11:16 +0700

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function disable(e, keyCode) {
 // f12, ctrl+u, ctrl+shift+i, console
 
 document.onkeydown = (e) => {
-    if (event.keyCode === 123 || ctrlShiftKey(e, 'I') || ctrlShiftKey(e, 'J') || ctrlShiftKey(e, 'C') || ctrlShiftKey(e, 'K') || (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)))
+    if (event.keyCode === 123 || disable(e, 'I') || disable(e, 'J') || disable(e, 'C') || disable(e, 'K') || (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)))
 
         return false;
 }
