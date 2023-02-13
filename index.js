1037514a324067d7302ac709665c4312a03e5d0b
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-02-13 10:58:03 +0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+// klik kanan
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function disable(e, keyCode){
+    return e.ctrlKey && e.shiftKey && e.keyCode == keyCode.charCodeAt(0);
+}
+
+// f12, ctrl+u, ctrl+shift+i
+
+document.onkeydown = (e) => {
+    if(event.keyCode === 123 || ctrlShiftKey(e, 'I') || ctrlShiftKey(e, 'J'), ctrlShiftKey(e, 'C') || (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)))
+
+    return false;
+}
