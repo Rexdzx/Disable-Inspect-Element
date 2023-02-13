95d57bc732c92bb7780b12dadbf4c49258091cce
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-02-13 12:06:49 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // klik kanan
 document.addEventListener('contextmenu', (e) => e.preventDefault());
 
-function disable(e, keyCode){
+function disable(e, keyCode) {
     return e.ctrlKey && e.shiftKey && e.keyCode == keyCode.charCodeAt(0);
 }
 
-// f12, ctrl+u, ctrl+shift+i
+// f12, ctrl+u, ctrl+shift+i, console
 
 document.onkeydown = (e) => {
-    if(event.keyCode === 123 || ctrlShiftKey(e, 'I') || ctrlShiftKey(e, 'J') || ctrlShiftKey(e, 'C') || (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)))
+    if (event.keyCode === 123 || ctrlShiftKey(e, 'I') || ctrlShiftKey(e, 'J') || ctrlShiftKey(e, 'C') || ctrlShiftKey(e, 'K') || (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)))
 
-    return false;
+        return false;
 }
