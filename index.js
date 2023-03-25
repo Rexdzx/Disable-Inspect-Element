edd29295d792b73979fa9c59d7b11e6b53623c97
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-03-26 05:05:49 +0700

    disable performance (shift + f5)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function disable(e, keyCode) {
     return e.ctrlKey && e.shiftKey && e.keyCode == keyCode.charCodeAt(0);
 }
 
-// f12, ctrl+u, ctrl+shift+i, console
+// f12, ctrl+u, ctrl+shift+i, console, shift + f5
 
 document.onkeydown = (e) => {
     if (event.keyCode === 123 || disable(e, 'I') || disable(e, 'J') || disable(e, 'C') || disable(e, 'K') || e.shiftKey === 116 || (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)))
